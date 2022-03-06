1111b25d85befe17a51c24c8054ceb0833a1a40a
1LeonardoCruz2 <98235843+1LeonardoCruz2@users.noreply.github.com>
2022-03-06 14:51:01 -0300

    Update relatorio.py

--- relatorio.py
+++ relatorio.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 vendas_df=pd.read_excel("Vendas.xlsx")
 dez_df=pd.read_excel("Vendas - Dez.xlsx")
 
-#adicionando o mes de dezembro ao nosso datafreime principla
+#adicionando o mes de dezembro ao nosso dataframe principal
 vendas_df=vendas_df.append(dez_df)
 
-#outra forma de terderminar a corelação entre as colunas é utilizando o pandas
+#fazendo o histograma dos valores
 vendas_df["Valor Final"].hist()
 plt.title("valor final")
 plt.savefig("hsitograma_final.png")
@@ -21,7 +21,7 @@ plt.savefig("hsitograma_uni.png")
 plt.show()
 
 #determinando a quantidade de trasações por loja
-trasacoes_por_loja=vendas_df["ID Loja"].value_counts()
+trasacoes_por_loja=vendas_df["ID Loja"].value_counts.to_frame()
 
 #criando um dataframe que contanhe a soma dos valores finais por loja
 faturamento_loja=vendas_df[["ID Loja","Valor Final"]].groupby("ID Loja").sum()
@@ -30,32 +30,32 @@ faturamento_loja=vendas_df[["ID Loja","Valor Final"]].groupby("ID Loja").sum()
 quantidade_loja=vendas_df[["ID Loja","Quantidade"]].groupby("ID Loja").sum()
 
 #verificando a media dos valor finais de acordo com a quantidade 
-ticket_medio=(faturamento_loja["Valor Final"]/quantidade_loja["Quantidade"]).to_frame()
-ticket_medio=ticket_medio.rename(columns={0:"ticket medio"})
+faturamento_medio=(faturamento_loja["Valor Final"]/quantidade_loja["Quantidade"]).to_frame()
+faturamento_medio=faturamento_medio.rename(columns={0:"ticket medio"})
 
-# enviando a analise feita por e-mail
+# local onde as imagens foram salvas
+anexo1 = fr'.../hsitograma_final.png' 
+anexo2 = fr'.../hsitograma_uni.png'
 
-anexo1 = fr'C:/Users/Csleo/Desktop/IFUSP/python/python/pandas/hsitograma_final.png'
-anexo2 = fr'C:/Users/Csleo/Desktop/IFUSP/python/python/pandas/hsitograma_uni.png'
-
+#enviando a analise feita por e-mail
 outlook=win32.Dispatch("outlook.application") #conectando o python ao outlook do computador
 mail=outlook.CreateItem(0) #criando um e-mail
-mail.To="l.c.pegasus12@gmail.com" #para quem vai o email
-mail.Subject= " teste do codigo " #assunto do email
-mail.Attachments.Add(anexo1)
-mail.Attachments.Add(anexo2)
+mail.To="" #endereço  de email do destinatário
+mail.Subject= " " #assunto do email
+mail.Attachments.Add(anexo1)  #anexando a imagem 1
+mail.Attachments.Add(anexo2)  #anexando a imagem 2
 mail.HTMLBody= f'''<p>prezado,</p>
 
                     <h1>segue relatorio automatico dos dados</h1>
 
-                    <p>telabe do faturament</p> 
+                    <p>faturament por loja</p> 
                     {faturamento_loja.to_html(formatters={"Valor Final":"R${:,.2f}".format})}
 
-                    <p>tabela da quantidade vendida por loja,</p> 
+                    <p>quantidade vendida por loja,</p> 
                     {quantidade_loja.to_html()}
 
-                    <p>tabela do ticket medio </p> 
-                    {ticket_medio.to_html(formatters={"ticket medio":"R${:,.2f}".format})} 
+                    <p>valor medio das compras por loja</p> 
+                    {faturamento_medio.to_html(formatters={"ticket medio":"R${:,.2f}".format})} 
 
                     <p>transações por loja </p> 
                     {trasacoes_por_loja.to_html()} 
